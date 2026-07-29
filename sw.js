501cffbb24f20036ffb036a17896e633e0f30444
jaalvarezs <90659351+jaalvarezs@users.noreply.github.com>
2026-07-28 20:44:35 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lab-chance-v19';
+const CACHE = 'lab-chance-v20';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html', './manifest.json'])));
   self.skipWaiting();
